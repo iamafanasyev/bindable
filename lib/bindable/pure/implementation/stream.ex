5de5ea6e07fbac283b8bfb4ec1b835e66f81397b
Alexander Afanasyev <90306702+iamafanasyev@users.noreply.github.com>
2023-05-04 00:47:46 +0800

    Update lib/bindable/pure/implementation/stream.ex
    Co-authored-by: Aleksandr Sysoev <sanchous.ok@gmail.com>

--- lib/bindable/pure/implementation/stream.ex
+++ lib/bindable/pure/implementation/stream.ex
@@ -1,4 +1,4 @@
 defimpl Bindable.Pure, for: Stream do
   @spec of(Enumerable.t(a), a) :: Enumerable.t(a) when a: var
-  def of(_, a), do: [a] |> Stream.cycle() |> Stream.take(1)
+  def of(_, a), do: %Stream{enum: [a]}
 end
